80879daa0cbf2b77385f497dbdc90eee08d4b51f
ummazannathrishad <83083158+ummazannathrishad@users.noreply.github.com>
2022-05-17 13:32:39 +0600

    login page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './assets/css/bootstrap.min.css';
+import './App.css';
+import Login from './components/login';
+
+function App() {
+  return (
+    <Login />
+  );
+}
+
+export default App;
---
+++ src/components/login.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import img from '../assets/image/login pic.png';
+
+const login = () => {
+  return (
+    <div className="login">
+    
+    <div className="container">
+        <div className="row">
+            <div className="col-md-12">
+                <div className="login-section shadow rounded-5 ">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="img border-end">
+                                <img src={img} alt="login" className="img-fluid"/>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <form action="" method="" className="form-section">
+                                <h3>Sign in</h3>
+                                <div className="mb-3">
+                                  <label for="exampleInputEmail1" className="form-label">Email address</label>
+                                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                  <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                </div>
+                                <div className="mb-3">
+                                  <label for="exampleInputPassword1" className="form-label">Password</label>
+                                  <input type="password" className="form-control" id="exampleInputPassword1"/>
+                                </div>
+                                <div className="mb-3 form-check">
+                                  <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                                  <label className="form-check-label" for="exampleCheck1">Remember me</label>
+                                </div>
+                                <button type="submit" className="btn button-skyblue">Submit</button>
+                              </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  
+    </div>
+  );
+}
+export default login;
